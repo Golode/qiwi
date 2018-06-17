7ca308a4e3753db73405ff3484d7e8943b67ffcf
Golode <40245166+Golode@users.noreply.github.com>
2018-06-17 18:31:28 +0300

    Update qiwi.py

--- qiwi.py
+++ qiwi.py
@@ -2,9 +2,7 @@ from SimpleQIWI import *
 drower = raw_input('Api:')
 api = QApi(token=drower, phone="")
 print(api.balance)
-pay = raw_input('Pay:')
-money = raw_input('$:')
-api.pay(account=rpay, amount=money)
+api.pay(account=raw_input('Pay:'), amount=raw_input('$:'))
 print(api.balance)
 exit = input("")
 
