b02d9faccb9c892bd8a50202aaab25b1de30dddc
Golode <40245166+Golode@users.noreply.github.com>
2018-06-24 18:03:29 +0300

    Update qiwi.py

--- qiwi.py
+++ qiwi.py
@@ -1,8 +1,41 @@
 from SimpleQIWI import *
-drower = raw_input('Api:')
-api = QApi(token=drower, phone="")
-print(api.balance)
-api.pay(account=raw_input('Pay:'), amount=raw_input('$:'))
-print(api.balance)
-exit = raw_input("exit")
-
+from colorama import init, Fore
+init()
+G = Fore.GREEN
+R = Fore.RED
+B = Fore.BLUE
+Y = Fore.YELLOW
+M = Fore.MAGENTA
+C = Fore.CYAN
+col ='''
+   ____ _ __    __ _ _        _              
+  /___ (_) / /\ \ (_) |_ ___ | | _____ _ __  
+ //  / / \ \/  \/ / | __/ _ \| |/ / _ \ '_ \ 
+/ \_/ /| |\  /\  /| | || (_) |   <  __/ | | |
+\___,_\|_| \/  \/ |_|\__\___/|_|\_\___|_| |_|
+################(Version:2.0)################
+|                  Author                   |
+|           JTexas   and    Paris302        |
+#############################################
+'''
+print (M,col,C)
+def user_info():
+	token=input('Token:')
+	api = QApi(token=token, phone="")
+	try:
+		print(G,api.balance,C)
+		pho=input('mob:+')
+		com=input('Comment:')
+		def pay_info():
+			try:	
+				api.pay(account=pho, amount=input('$:'), currency=com) 
+			except:
+				print(R,'Error:Money',C)
+				return pay_info()
+		pay_info()
+		print(G,api.balance,C)
+	except:
+		print(R,'Error:Token',C)
+		return user_info()
+user_info()
+input('Exit')
